cda2900f3f86e00c201a46c76d5e2b0bc7f955e3
alechuang98 <alechuang98@gmail.com>
2020-05-14 15:34:53 +0800

    baseline

---
+++ src/cluster.py
@@ -0,0 +1,37 @@
+import torch
+from sklearn.decomposition import KernelPCA
+from sklearn.manifold import TSNE
+from sklearn.cluster import MiniBatchKMeans
+from torch.utils.data import DataLoader
+from data import *
+
+def inference(X, model, batch_size=256):
+    X = preprocess(X)
+    dataset = Image_Dataset(X)
+    dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=False)
+    latents = []
+    for i, x in enumerate(dataloader):
+        x = torch.FloatTensor(x)
+        vec, img = model(x.cuda())
+        if i == 0:
+            latents = vec.view(img.size()[0], -1).cpu().detach().numpy()
+        else:
+            latents = np.concatenate((latents, vec.view(img.size()[0], -1).cpu().detach().numpy()), axis = 0)
+    print('Latents Shape:', latents.shape)
+    return latents
+
+def predict(latents):
+    # First Dimension Reduction
+    transformer = KernelPCA(n_components=200, kernel='rbf', n_jobs=-1)
+    kpca = transformer.fit_transform(latents)
+    print('First Reduction Shape:', kpca.shape)
+
+    # # Second Dimesnion Reduction
+    X_embedded = TSNE(n_components=2).fit_transform(kpca)
+    print('Second Reduction Shape:', X_embedded.shape)
+
+    # Clustering
+    pred = MiniBatchKMeans(n_clusters=2, random_state=0).fit(X_embedded)
+    pred = [int(i) for i in pred.labels_]
+    pred = np.array(pred)
+    return pred, X_embedded
---
+++ src/main.py
@@ -0,0 +1,140 @@
+import torch
+import glob
+from torch import optim
+import numpy as np
+import matplotlib.pyplot as plt
+import torch.nn as nn
+from torch.utils.data import DataLoader
+from data import *
+from utils import *
+from model import AE
+from cluster import *
+
+TRAIN = 1
+TEST = 1
+HW = 1
+EPOCH = 100
+
+same_seeds(0)
+
+trainX = np.load('./data/trainX.npy')
+trainX_preprocessed = preprocess(trainX)
+
+if TRAIN:
+    img_dataset = Image_Dataset(trainX_preprocessed)
+
+    model = AE().cuda()
+    criterion = nn.MSELoss()
+    optimizer = torch.optim.Adam(model.parameters(), lr=1e-5, weight_decay=1e-5)
+
+    model.train()
+    n_epoch = EPOCH
+
+    # 準備 dataloader, model, loss criterion 和 optimizer
+    img_dataloader = DataLoader(img_dataset, batch_size=64, shuffle=True)
+
+    # 主要的訓練過程
+    for epoch in range(n_epoch):
+        for data in img_dataloader:
+            img = data
+            img = img.cuda()
+
+            output1, output = model(img)
+            loss = criterion(output, img)
+            
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+            if (epoch+1) % 10 == 0:
+                torch.save(model.state_dict(), './checkpoints/checkpoint_{}.pth'.format(epoch+1))
+                
+        print('epoch [{}/{}], loss:{:.5f}'.format(epoch+1, n_epoch, loss.data))
+
+    # 訓練完成後儲存 model
+    torch.save(model.state_dict(), './checkpoints/last_checkpoint.pth')
+
+
+if TEST:
+    # load model
+    model = AE().cuda()
+    model.load_state_dict(torch.load('./checkpoints/last_checkpoint.pth'))
+    model.eval()
+
+    # 準備 data
+    trainX = np.load('./data/trainX.npy')
+
+    # 預測答案
+    latents = inference(X=trainX, model=model)
+    pred, X_embedded = predict(latents)
+
+    # 將預測結果存檔，上傳 kaggle
+    save_prediction(pred, 'prediction.csv')
+
+    # 由於是 unsupervised 的二分類問題，我們只在乎有沒有成功將圖片分成兩群
+    # 如果上面的檔案上傳 kaggle 後正確率不足 0.5，只要將 label 反過來就行了
+    save_prediction(invert(pred), 'prediction_invert.csv')
+
+if HW:
+    valX = np.load('./data/valX.npy')
+    valY = np.load('./data/valY.npy')
+
+    # ==============================================
+    #  我們示範 basline model 的作圖，
+    #  report 請同學另外還要再畫一張 improved model 的圖。
+    # ==============================================
+    model.load_state_dict(torch.load('./checkpoints/last_checkpoint.pth'))
+    model.eval()
+    latents = inference(valX, model)
+    pred_from_latent, emb_from_latent = predict(latents)
+    acc_latent = cal_acc(valY, pred_from_latent)
+    print('The clustering accuracy is:', acc_latent)
+    print('The clustering result:')
+    plot_scatter(emb_from_latent, valY, savefig='p1_baseline.png')
+
+    plt.figure(figsize=(10,4))
+    indexes = [1,2,3,6,7,9]
+    imgs = trainX[indexes,]
+    for i, img in enumerate(imgs):
+        plt.subplot(2, 6, i+1, xticks=[], yticks=[])
+        plt.imshow(img)
+    # 畫出 reconstruct 的圖
+    inp = torch.Tensor(trainX_preprocessed[indexes,]).cuda()
+    latents, recs = model(inp)
+    recs = ((recs+1)/2 ).cpu().detach().numpy()
+    recs = recs.transpose(0, 2, 3, 1)
+    for i, img in enumerate(recs):
+        plt.subplot(2, 6, 6+i+1, xticks=[], yticks=[])
+        plt.imshow(img)
+    plt.tight_layout()
+    plt.savefig('p2_result.png')
+    plt.show()
+
+    checkpoints_list = sorted(glob.glob('checkpoints/checkpoint_*.pth'))
+    # load data
+    dataset = Image_Dataset(trainX_preprocessed)
+    dataloader = DataLoader(dataset, batch_size=64, shuffle=False)
+    points = []
+    with torch.no_grad():
+        for i, checkpoint in enumerate(checkpoints_list):
+            print('[{}/{}] {}'.format(i+1, len(checkpoints_list), checkpoint))
+            model.load_state_dict(torch.load(checkpoint))
+            model.eval()
+            err = 0
+            n = 0
+            for x in dataloader:
+                x = x.cuda()
+                _, rec = model(x)
+                err += torch.nn.MSELoss(reduction='sum')(x, rec).item()
+                n += x.flatten().size(0)
+            print('Reconstruction error (MSE):', err/n)
+            latents = inference(X=valX, model=model)
+            pred, X_embedded = predict(latents)
+            acc = cal_acc(valY, pred)
+            print('Accuracy:', acc)
+            points.append((err/n, acc))
+    ps = list(zip(*points))
+    plt.figure(figsize=(6,6))
+    plt.subplot(211, title='Reconstruction error (MSE)').plot(ps[0])
+    plt.subplot(212, title='Accuracy (val)').plot(ps[1])
+    plt.savefig('p3_result.png')
+    plt.show()
